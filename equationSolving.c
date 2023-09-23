7f091cc6ea6e0a3f594f39de19582849a1f6c18c
31n5t31n <53md3v@proton.me>
2023-09-23 23:49:53 +0200

    Rafetal

--- equationSolving.c
+++ equationSolving.c
@@ -3,30 +3,31 @@
 int main()
 {
     float a,b,c,delta;
-    printf("*****Ce programme vous permet de resoudre des equations du second degres*****");
-    printf("Veuillez entrez la premiere valeur");
+    printf("*****Ce programme vous permet de resoudre des equations du second degres*****\n");
+    printf("Veuillez entrez la premiere valeur\t");
     scanf("%f",&a);
     do
     {
-        /* code */printf("desole la premiere valeur ne peut etre nulle");
+        printf("desole la premiere valeur ne peut etre nulle\t");
+        scanf("%f",&a);
     } while (a==0);
-    printf("Veuillez entrez les deux autres valeurs");
+    printf("Veuillez entrez les deux autres valeurs\t");
     scanf("%f%f",&b,&c);
-    printf("Votre equqtion est de la forme %fx2+%fx+%f", a,b,c);
+    printf("Votre equation est de la forme %fx2+%fx+%f\n", a,b,c);
     delta=b*b-4*a*c;
     if(delta==0)
-        printf("Votre equqtion admet une solution double x0= %.2f", -b/2*a);
+        printf("Votre equqtion admet une solution double x0= %.2f\n", -b/2*a);
     else if (delta>0)
     {
         float x1,x2;
         x1= (-b- sqrt(delta) ) /2*a;
         x2= (-b+ sqrt(delta) ) /2*a;
-        printf("Votre equation admet deux solutions x1 = %.2f  et x2 = %.2f",x1,x2);
+        printf("Votre equation admet deux solutions x1 = %.2f  et x2 = %.2f\n",x1,x2);
     }    
     else
     {
         char r;
-        printf("Votre equation n'admet pas de solution dans R, souhaitez-vous voir les solutions dans C (Y/N)");
+        printf("Votre equation n'admet pas de solution dans R, souhaitez-vous voir les solutions dans C (Y/N)\n");
         fflush (stdin);
         r=getchar();
         if(r== 'Y')
