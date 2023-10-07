dd1efa4e2eb8bb5e9567abb90ee023e3e874ef0a
Niang Seydina Oumar <niang.e2203746@etud.univ-ubs.fr>
2023-10-08 00:29:07 +0200

    update c library

--- equationSolving.c
+++ equationSolving.c
@@ -1,45 +1,45 @@
-#include<stdio.h>
-#include<math.h>
+// quadratic.c
+#include "quadratic.h"
+#include <stdio.h>
+#include <math.h>
 
-void eqnSolving()
-{
-    float a,b,c,delta;
-    printf("*****Ce programme vous permet de resoudre des equations du second degres*****\n");
-    printf("Veuillez entrez la premiere valeur\t");
-    scanf("%f",&a);
-    while (a==0)
-    {
-        printf("desole la premiere valeur ne peut etre nulle\t");
-        scanf("%f",&a);
+float getInputA() {
+    float a;
+    printf("Veuillez entrer la première valeur\t");
+    scanf("%f", &a);
+    while (a == 0) {
+        printf("Désolé, la première valeur ne peut pas être nulle\t");
+        scanf("%f", &a);
     }
-    printf("Veuillez entrez les deux autres valeurs\t");
-    scanf("%f%f",&b,&c);
-    printf("Votre equation est de la forme %.2fx2+%.2fx+%.2f\n", a,b,c);
-    delta=b*b-4*a*c;
-    if(delta==0)
-        printf("Votre equation admet une solution double x0= %.2f\n", -b/2*a);
-    else if (delta>0)
-    {
-        float x1,x2;
-        x1= (-b- sqrt(delta) ) /(2*a);
-        x2= (-b+ sqrt(delta) ) /(2*a);
-        printf("Votre equation admet deux solutions x1 = %.2f  et x2 = %.2f\n",x1,x2);
-    }    
-    else
-    {
-        char r;
-        printf("Votre equation n'admet pas de solution dans R, souhaitez-vous voir les solutions dans C (Y/N)\n");
-        fflush (stdin);
-        r=getchar();
-        if(r== 'Y' || r=='y')
-            printf(".............FORGET FORMULAS..............");
-        else if (r=='N' || r=='n')
-            printf(".........See you soon............");
-    }
-    
+    return a;
 }
-int main()
-{
-    eqnSolving();
-    return 0;
+
+void getInputBC(float *b, float *c) {
+    printf("Veuillez entrer les deux autres valeurs\t");
+    scanf("%f %f", b, c);
+}
+
+float calculateDiscriminant(float a, float b, float c) {
+    return b * b - 4 * a * c;
+}
+
+void solveQuadratic(float a, float b, float c) {
+    float delta = calculateDiscriminant(a, b, c);
+    if (delta == 0) {
+        printf("Votre équation admet une solution double x0 = %.2f\n", -b / (2 * a));
+    } else if (delta > 0) {
+        float x1 = (-b - sqrt(delta)) / (2 * a);
+        float x2 = (-b + sqrt(delta)) / (2 * a);
+        printf("Votre équation admet deux solutions x1 = %.2f et x2 = %.2f\n", x1, x2);
+    } else {
+        char r;
+        printf("Votre équation n'admet pas de solution dans R, souhaitez-vous voir les solutions dans C (Y/N)\n");
+        fflush(stdin);
+        r = getchar();
+        if (r == 'Y' || r == 'y') {
+            printf(".............FORGET FORMULAS..............");
+        } else if (r == 'N' || r == 'n') {
+            printf(".........À bientôt............");
+        }
+    }
 }
